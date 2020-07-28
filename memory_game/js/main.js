d0297fa2d7c37e069676592a6ab6d832a506e88d
Hovanec Joseph <HovanecJoseph@JohnDeere.com>
2020-07-27 20:25:46 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,26 @@
 console.log("Up and running!")
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
+//Create set of cards
+let cards = ["queen","queen","king","king"];
+
+//Create empty array for flipped set
+let cardsInPlay = [];
+
+//Define first card flipped
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+//console.log(cardsInPlay);
+
+//Define second card flipped
+let cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+
+//Determine if cards are matching and create pop up message
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
