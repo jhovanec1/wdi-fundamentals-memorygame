7e5fdbd724da06be5ed5fb662eb18af9bd4192f6
Hovanec Joseph <HovanecJoseph@JohnDeere.com>
2020-07-29 13:25:15 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,28 +1,64 @@
 console.log("Up and running!")
 //Create set of cards
-let cards = ["queen","queen","king","king"];
+let cards = [ 
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 
 //Create empty array for flipped set
 let cardsInPlay = [];
 
 
 function checkForMatch() {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You found a match!");
+	
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!");
+		} else {
+			alert("Sorry, try again.");
+		}
 	} else {
-		console.log("Sorry, try again.");
+		return;
 	}
-
 }
 
 
-function flipCard(cardId) {
-	
-	cardsInPlay.push(cards[cardId]);
-	
-	console.log("User flipped " + cards[cardId]);
-	//add if statement so doesnt always check for match if array length != 2
+function flipCard() {
+	let cardId = this.getAttribute('data-id');
+	this.setAttribute('src' , cards[cardId].cardImage);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 	checkForMatch();
 }
-flipCard(0);
-flipCard(0);
+
+
+function createBoard() {
+	for (let i = 0; i < cards.length; i++) {
+		let cardElement = document.createElement('img');
+		cardElement.setAttribute('src',"images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+createBoard();
